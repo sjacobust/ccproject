594f2d319e02b046a7865a36aad36f930cc9f8da
OverripeWings3 <sjacobust@gmail.com>
2021-05-06 15:27:19 -0500

    Added Services and New Views

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  loginInfo: any = {
+    Email: "",
+    Password: ""
+  };
+
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+  }
+
+  login () {
+    const allUsers = this.userService.getUsers()
+    console.log(allUsers);
+    this.loginInfo = {
+      
+    }
+    this.userService.loginUser(this.loginInfo)
   }
 
 }
